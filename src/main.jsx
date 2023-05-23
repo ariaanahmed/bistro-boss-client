a7ee69d2e207898dc9b361992711d35bb13245ba
ariaan ahmed <you@example.com>
2023-05-23 11:03:53 +0600

    74-3 Create simple Top Banner react responsive carousel

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
+import { router } from './routes/Routes';
+
+import {
+  RouterProvider,
+} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <div className='max-w-screen-2xl mx-auto'>
+      <RouterProvider router={router} />
+    </div>
   </React.StrictMode>,
 )
